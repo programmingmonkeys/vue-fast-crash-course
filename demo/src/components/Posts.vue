c41ae9a77771f6173d24d8fb8899e6427e042fe6
codingChewie <codingChewie@gmail.com>
2020-10-16 09:11:22 -0500

    25. HTTP

--- demo/src/components/Posts.vue
+++ demo/src/components/Posts.vue
@@ -1,17 +1,44 @@
 <template>
-  <div>
-    {{ message }}
-    <button class="btn btn-primary">Click</button>
+  <div class="container">
+    <table class="table table-striped mt-4">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">User Id</th>
+          <th scope="col">Title</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.userId }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.body }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'PostExample',
   data() {
     return {
-      message: 'this is a test'
+      posts: []
     }
+  },
+
+  created() {
+    console.log('test')
+
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((posts) => {
+      this.posts = posts.data
+      console.log(posts.data)
+    })
   }
 }
 </script>
